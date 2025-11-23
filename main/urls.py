c10bd20605b5cc153be3fdc01976b4f06dc0dd5a
karimov <devizzatillo@gmail.com>
2025-11-23 22:45:05 +0500

    adding new things

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from .views import CreateProductView, UpdateProductView, Home 
+from .views import CreateProductView, UpdateProductView, Home, DetailProducView,DeleteProductView
 
 urlpatterns = [ 
-	path("", Home.as_view(), name="home"), 
+	path("", Home.as_view(), name="home"),
 	path('create/', CreateProductView.as_view(), name='create'), 
-	path('update/<int:pk>', UpdateProductView.as_view(), name='update')
+	path('detail/<int:pk>', DetailProducView.as_view(), name='detail'),
+	path('update/<int:pk>', UpdateProductView.as_view(), name='update'),
+	path('delete/<int:pk>', DeleteProductView.as_view(), name='delete')
 ]
--- main/views.py
+++ main/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView 
-from django.views.generic.edit import CreateView, DeleteView, UpdateView 
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Product
 
-class Home(TemplateView):
+class Home(ListView):
 	model = Product
 	template_name = 'index.html'
+	context_object_name = 'products'
 
-
-class  CreateProductView(CreateView): 
+class CreateProductView(CreateView): 
 	model = Product
 	template_name = 'create.html'
+	context_object_name = 'product'
 	fields = ['name', 'desc', 'price', 'image']
 	success_url = reverse_lazy('home')
 
 
 class UpdateProductView(UpdateView):
 	model = Product 
-	template_name = "update.html" 
+	template_name = "update.html"
+	context_object_name = 'product'
 	fields = ['name', 'desc', 'price', 'image'] 
+	success_url = reverse_lazy('home')
+
+
+class DetailProducView(DetailView): 
+	model = Product 
+	template_name = 'detail.html'
+	context_object_name = 'product'
+
+
+class DeleteProductView(DeleteView): 
+	model = Product
+	template_name = 'delete.html' 
+	context_object_name = 'product'
 	success_url = reverse_lazy('home')
